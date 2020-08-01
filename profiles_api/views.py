d6ebe6f7f5bafb426af6f4eee2ec4943509a6327
ppk <pwint.phyu21@gmail.com>
2020-08-01 12:43:00 +0800

    Added HellowApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here
-#testing for ppk:.
+
+class HelloApiView(APIView):
+    """Test Api View"""
+
+    def get(self, request, format=None):
+        """Returns  a lis"""
+        result =[
+            'a',
+            'b',
+            'c',
+            'd',
+        ]
+
+        return Response({"message":"Hello","an_apiview": result})
