86aec9a3c04cd317474808e6d5c96ac1e96a338b
ppk <pwint.phyu21@gmail.com>
2020-08-02 21:54:54 +0800

    Adding login TokenAuthentication

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,8 +8,10 @@ router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile',views.UserProfileViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('hello-view/', views.HelloApiView.as_view()),
-    
+    path('login/', views.UserLoginApiView.as_view()),
+    path('', include(router.urls))
+
+
 
 ]
