c9c82b379a02a0291d12aa6d632549e7ebfa9eac
ppk <pwint.phyu21@gmail.com>
2020-08-02 19:04:47 +0800

    Added user profiles user api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
+from profiles_api import permissions
 from profiles_api import models
 from profiles_api import serializers
 
@@ -103,3 +105,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
